01e77f6ec2bbbdfcc1ca10cca3139d68e5fdd667
Avearen <x5n4@pm.me>
2023-08-02 09:45:58 +0200

    Vue added header

--- my-frontend/src/main.js
+++ my-frontend/src/main.js
@@ -1,5 +1,10 @@
+// * DEFAULT IMPORTS
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// * ADDITIONAL IMPORTS
+import 'bootstrap/dist/css/bootstrap.css'
+
 
 createApp(App).use(router).mount('#app')
