e3eb4007adc5e5c081ce5d9ec312db41179cb5b5
Avearen <x5n4@pm.me>
2023-08-05 01:34:09 +0300

    New chaneges

---
+++ ao-project/src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app-bar color="">
+        <v-toolbar-title>AutoOperator</v-toolbar-title>
+
+    </v-app-bar>
+</template>
+
+
+  
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+  
---
+++ ao-project/src/views/LandingPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <NavBar />
+        <!-- Other code for your landing page goes here -->
+    </div>
+</template>
+  
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+    components: {
+        NavBar
+    }
+}
+</script>
+  
